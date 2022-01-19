31a29dd4c67f4f3239eb5b30f8a842f07819a804
nato000 <vganja2@gmail.com>
2022-01-19 18:47:54 +0200

    add first clickable card for goal-card section

--- js/card-link.js
+++ js/card-link.js
@@ -1,6 +1,10 @@
-const card = document.querySelector(".card");
-const mainLink = card.querySelector(".main-link");
-const clickableElements = Array.from(card.querySelectorAll("a")); //we are using 'a' here for simplicity but ideally you should put a class like 'clickable' on every clicable element inside card(a, button) and use that in query selector
+const card = document.querySelector(".goal-card");
+const mainLink = card.querySelector(".goal-card__link");
+const clickableElements = Array.from(card.querySelectorAll("a"));
+
+// we are using 'a' here for simplicity but ideally you should put a class like
+// 'clickable' on every clicable element inside card(a, button) and use that
+// in query selector
 
 clickableElements.forEach((ele) => ele.addEventListener("click", (e) => e.stopPropagation()));
 
